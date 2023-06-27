9e646d2479982fc70c1e1fa5a4e618545e0fdba3
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-06-27 14:16:33 +0700

    Create Increasing Triple Subsequence.py

---
+++ Increasing Triple Subsequence.py
@@ -0,0 +1,18 @@
+#nums = [2^31-1] + nums + [-2^31]
+#iteration with pointer before and after point
+
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        first = second = float('inf')
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False

b6816038744d793072655eb78f7f02f397c2a587
Aditya Tresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2023-06-27 17:24:57 +0700

    Create Move Zeroes.py

---
+++ Two Pointers/Move Zeroes.py
@@ -0,0 +1,18 @@
+#iterate through array
+#pointer i -> marks 0
+#pointer j -> mmarks non 0
+#gradually movind 0 to the back
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            else:
+                pass
